a75672a158764d890f1c7dcc900e2db19bf69084
Thiago-Nascimento <thiago.r.nascimento@hotmail.com>
2024-01-09 18:54:39 -0300

    feat: fim aula 3 - Criação de Mapas e Game Loop

--- Overworld.js
+++ Overworld.js
@@ -3,33 +3,37 @@ class Overworld {
         this.element = config.element;
         this.canvas = this.element.querySelector(".game-canvas")
         this.ctx = this.canvas.getContext("2d")
+        this.map = null
+    }
+
+    startGameLoop() {
+        const step = () => {
+            // Limpa o canvas antes de desenhar a cada frameUpdate
+            this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+
+            // Desenha a camada de baixo
+            this.map.drawLowerImage(this.ctx)
+
+            // Desenha os GameObjects - Utiliza o Object.values pois não é um array, é um objeto, transforma em array as propriedades de um objeto
+            Object.values(this.map.gameObjects).forEach(object => {
+                object.x += 0.03
+                object.sprite.draw(this.ctx)
+            })
+
+            // Desenha a camada de cima
+            this.map.drawUpperImage(this.ctx)
+
+            requestAnimationFrame(() => {
+                step()
+            })
+        }
+
+        step()
     }
 
     init() {
-        const image = new Image();
-        image.src = "/images/maps/DemoLower.png"
-
-        image.onload = () => {
-            this.ctx.drawImage(image, 0, 0)
-        }
-
-        // Adicionando os game objects
-        const hero = new GameObject({
-            x: 5,
-            y: 6,
-            
-        })
-
-        const npc1 = new GameObject({
-            x: 7,
-            y: 9,
-            src: "/images/characters/people/npc1.png"
-        })
-
-        setTimeout(() => {
-            hero.sprite.draw(this.ctx);
-            npc1.sprite.draw(this.ctx);
-        }, 200)
+        this.map = new OverworldMap(window.OverworldMaps.Kitchen)
+        this.startGameLoop()
 
     }
 }
