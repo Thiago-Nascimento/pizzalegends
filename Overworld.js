5675d1aa22926b3ea16ef4d76618042976548441
Thiago-Nascimento <thiago.r.nascimento@hotmail.com>
2024-01-09 15:55:30 -0300

    feat: fim aula 1 - Personagem e sombra adicionados

--- Overworld.js
+++ Overworld.js
@@ -12,5 +12,43 @@ class Overworld {
         image.onload = () => {
             this.ctx.drawImage(image, 0, 0)
         }
+
+
+        const x = 5
+        const y = 6
+
+        const shadow = new Image()
+        shadow.src = "/images/characters/shadow.png"
+
+        shadow.onload = () => {
+            this.ctx.drawImage(
+                shadow,   // Objeto da imagem
+                0,  // x de inicio do corte (crop)
+                0,  // y de inicio do corte (crop)
+                32, // largura de corte
+                32, // altura de corte
+                x * 16 - 8,  // coordenada x
+                y * 16 - 18,  // coordenada y
+                32, // largura à desenhar
+                32  // altura à desenhar;
+            )
+        }
+
+        const hero = new Image();
+        hero.src = "/images/characters/people/hero.png"
+        
+        hero.onload = () => {
+            this.ctx.drawImage(
+                hero,   // Objeto da imagem
+                0,  // x de inicio do corte (crop)
+                0,  // y de inicio do corte (crop)
+                32, // largura de corte
+                32, // altura de corte
+                x * 16 - 8,  // coordenada x
+                y * 16 - 18,  // coordenada y
+                32, // largura à desenhar
+                32  // altura à desenhar;
+            )
+        }
     }
 }
